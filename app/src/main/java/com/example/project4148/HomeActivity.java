19b0022742d418afde590f393258cf14d32ab106
Gokul Ramasamy <69314159+Gokul-Rams@users.noreply.github.com>
2020-11-16 19:18:40 +0530

    Login activites updated

--- app/src/main/java/com/example/project4148/HomeActivity.java
+++ app/src/main/java/com/example/project4148/HomeActivity.java
@@ -14,40 +14,86 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Trace;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     NavigationView navview;
     DrawerLayout drawer;
-    ConstraintLayout navheader;
+    FrameLayout navheader;
     Toolbar toolbar;
     FragmentManager fragmentManager;
+    FirebaseAuth auth;
+    TextView tvheaderusername,tvheadercurrentlocation,addeditprofile;
+    Button btnsignout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        toolbar = findViewById(R.id.home_toolbar);
-        drawer = findViewById(R.id.homenav_drawer);
-        navview = findViewById(R.id.home_navigetionniew);
-        navheader = (ConstraintLayout) navview.getHeaderView(0);
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        if(user==null){
+            Intent intent = new Intent(HomeActivity.this,LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            toolbar = findViewById(R.id.home_toolbar);
+            drawer = findViewById(R.id.homenav_drawer);
+            navview = findViewById(R.id.home_navigetionniew);
+            navheader = (FrameLayout) navview.getHeaderView(0);
+            tvheaderusername = navheader.findViewById(R.id.tvheaderusername);
+            tvheadercurrentlocation = navheader.findViewById(R.id.tvheadercurrentlocation);
+            btnsignout = navheader.findViewById(R.id.btnsignout);
+            addeditprofile = navheader.findViewById(R.id.addeditprofile);
 
-        setSupportActionBar(toolbar);
+            setSupportActionBar(toolbar);
 
-        navview.setNavigationItemSelectedListener(this);
+            navview.setNavigationItemSelectedListener(this);
 
-        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(HomeActivity.this,drawer,toolbar,R.string.drawer_open,R.string.drawer_close);
-        drawer.addDrawerListener(toogle);
-        toogle.syncState();
+            ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(HomeActivity.this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+            drawer.addDrawerListener(toogle);
+            toogle.syncState();
 
-        fragmentManager = getSupportFragmentManager();
-        FragmentTransaction trans = fragmentManager.beginTransaction();
-        HomeFragment fragtoadd = new HomeFragment();
-        trans.replace(R.id.fragmentcontainer,fragtoadd);
-        trans.commit();
+            fragmentManager = getSupportFragmentManager();
+            FragmentTransaction trans = fragmentManager.beginTransaction();
+            HomeFragment fragtoadd = new HomeFragment();
+            trans.replace(R.id.fragmentcontainer, fragtoadd);
+            trans.commit();
+
+            tvheaderusername.setText("Hello " + user.getEmail());
+
+            btnsignout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    auth.signOut();
+                    Intent intent = new Intent(HomeActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+
+            addeditprofile.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(HomeActivity.this,AdddetailsActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
     }
 
     @Override
